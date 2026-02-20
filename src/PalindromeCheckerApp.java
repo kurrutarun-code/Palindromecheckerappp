f3be0c450c317e98f183b3f9868fec600dc6495e
kurrutarun <kurrutarun@gmail.com>
2026-02-20 10:32:51 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,11 @@
 public class PalindromeCheckerApp{
     public static void main(String[] args){
 
+        System.out.println("=================================");
+        System.out.println("      PALINDROME CHECKER APP     ");
+        System.out.println("=================================");
+        System.out.println("Version: 1.0.0");
+        System.out.println("Developed for Java OOP Practice");
+        System.out.println("\nApplication is ready...\n");
     }
 }
